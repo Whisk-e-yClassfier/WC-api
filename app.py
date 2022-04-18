43a274fd57eacf0f0acc7d60eb9be1277e725180
BobbyLeeSH <tkdgus1797@gmail.com>
2022-04-19 00:49:30 +0900

    add readme

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ if __name__ == "__main__":
 
 @app.route("/")
 def hello_world():
-    # config = '../mmdetection/configs/hrnet/fcos_hrnetv2p_w32_gn-head_mstrain_640-800_4x4_2x_coco.py'
-
     config_file = './model/testconfig_v2.py'
-    # checkpoint_file = './model/fcos_hrnetv2p_w40_gn-head_mstrain_640-800_4x4_2x_coco_20201212_124752-f22d2ce5.pth'
     checkpoint_file = './model/latest_v2.pth'
     model = init_detector(config_file, checkpoint_file, device='cpu')
     img = './balvenie_test.jpg'
